567f605e56eb012b0ec36a942edea44db2fa8ff6
Sourav Roy <sourav.roy0407@gmail.com>
2026-07-06 21:25:13 +0530

    chore: bump version to 1.0.1 and add long_description

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="noirag",
-    version="1.0.0",
+    version="1.0.1",
     description="Noise-Aware Retrieval-Augmented Generation preprocessing engine",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Sourav Roy & Team NoiRAG",
     packages=find_packages(include=["noirag", "noirag.*"]),
     install_requires=[
